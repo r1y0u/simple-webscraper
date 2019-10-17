4a62c6f2ac990fef1a057368de2d3318935d460e
ryu <loved.baske14pride10ryu@gmail.com>
2019-10-17 20:19:01 +0900

    initial commit

--- webscraper.py
+++ webscraper.py
@@ -8,11 +8,10 @@ class Scraper:
         self.site = site
 
     def scrape(self):
-        r = urllib.request\
-            .urlopen(self.site)
+        r = urllib.request.urlopen(self.site)
         html = r.read()
         parser = "html.parser"
-        sp = BeautifulSoup(html,arser)
+        sp = BeautifulSoup(html,parser)
         for tag in sp.find_all("a"):
             url = tag.get("href")
             if url is None:
